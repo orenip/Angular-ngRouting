c7244a852875c64c28062f4810f85ff46b95a9f4
Jose Antonio López Piñero <josanlopi@gmail.com>
2023-04-07 13:45:03 +0200

    7-4-23 /2

--- src/app/pages/random-contact-page/random-contact-page.component.ts
+++ src/app/pages/random-contact-page/random-contact-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IRandomContact, Results } from 'src/app/models/randomuser';
+import { RandomUserService } from 'src/app/services/random-user.service';
 
 @Component({
   selector: 'app-random-contact-page',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./random-contact-page.component.scss']
 })
 export class RandomContactPageComponent {
+
+  randomContact: IRandomContact| undefined;
+
+  constructor(private randomUserService: RandomUserService){}
+
+  ngOnInit(): void {
+    this.randomUserService.obtenerRandomContact().subscribe((response: Results)=>{
+
+      this.randomContact= response.results[0];
+      console.table(this.randomContact.name);
+    })
+
+  }
 
 }
